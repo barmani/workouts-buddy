9210df9ae304cf4ca7ba89336cee9d7125d8ec2e
Brendan Armani <brendanarmani@gmail.com>
2018-05-13 20:59:40 -0400

    Minor styling to form

--- assets/app/new-workout/new-workout.component.ts
+++ assets/app/new-workout/new-workout.component.ts
@@ -10,7 +10,8 @@ enum Question {
 }
 @Component({
     selector: 'app-new-workout',
-    templateUrl: './new-workout.component.html'
+    templateUrl: './new-workout.component.html',
+    styleUrls: ['./new-workout.component.css']
 })
 export class NewWorkoutComponent {
 
