86fada4e52b8b7f0b3167ed0bfa87e18abeb0dcf
Brendan Armani <brendanarmani@gmail.com>
2018-06-29 12:03:02 -0400

    Reactive form for set forms added

--- src/app/my-workout/set/set.component.ts
+++ src/app/my-workout/set/set.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { FormControl, FormGroup } from '@angular/forms';
 
 import { Exercise } from '../../models/exercise.model';
 import { MyWorkoutService } from '../my-workout.service';
@@ -19,10 +20,25 @@ export class SetComponent implements OnInit {
   @Input() setArrayLength: number; // have this to make sure the user cannot add more than 6 sets
   @Output() changeAmountOfSets: EventEmitter<{add: boolean, index: number}> = new EventEmitter<{add: boolean, index: number}>();
 
+  repsFormControl = new FormControl();
+  weightFormControl = new FormControl();
+  unitOfMeasureFormControl = new FormControl();
+  setForm: FormGroup
   unitsOfMeasure = ['LBS', 'KG'];
   repOptions = Array.from({length: 100}, (v, k) => k+1);
 
   ngOnInit() {
+    this.repsFormControl.setValue(this.reps);
+    this.weightFormControl.setValue(this.weight);
+    this.unitOfMeasureFormControl.setValue(this.unitOfMeasure);
+    this.setForm = new FormGroup({
+      reps: this.repsFormControl,
+      weight: this.weightFormControl,
+      unitOfMeasure: this.unitOfMeasureFormControl
+    });
+    this.setForm.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
   }
 
   addSet() {
