d3fe7615c51f4275e9db1ed3c4b9522a1888355d
Brendan Armani <brendanarmani@gmail.com>
2018-07-15 10:59:48 -0400

    Added fields to change password on user page (still need to implement service and backend), only can access user page if signed in as that user

--- src/app/login-signup/login-signup.service.ts
+++ src/app/login-signup/login-signup.service.ts
@@ -65,7 +65,6 @@ export class LoginSignupService {
           const result = response.json();
         })
       );
-
   }
 
 }
--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { LoginSignupService } from '../login-signup/login-signup.service';
 import { Workout } from '../models/workout.model';
@@ -10,18 +12,34 @@ import { Subscription } from 'rxjs';
   templateUrl: './user-page.component.html'
 })
 export class UserPageComponent implements OnInit {
-  username: string;
+  username: string = localStorage.getItem('username');
   userId: string = localStorage.getItem('userId');
   subscription: Subscription;
+  changePasswordFormGroup: FormGroup;
 
-  constructor(private loginSignupService: LoginSignupService) {}
-
-  ngOnInit() {
+  constructor(private loginSignupService: LoginSignupService,
+              private fb: FormBuilder,
+              private router: Router,
+              private route: ActivatedRoute) {
     this.subscription = this.loginSignupService.getLoginObservable()
       .subscribe(loginInfo => {
         this.userId = loginInfo.userId;
         this.username = loginInfo.username;
       });
-    this.loginSignupService.getUserPage(this.userId).subscribe();
+  }
+
+  ngOnInit() {
+    // only access this page if the user is signed in and it is their own page
+    if (!this.userId || this.userId != this.route.snapshot.params['id']) {
+      this.router.navigateByUrl('/login-signup?token=invalid');
+    }
+    this.changePasswordFormGroup = this.fb.group({
+      oldPassword: new FormControl(),
+      newPassword: new FormControl(),
+      newPasswordConfirm: new FormControl()
+    });
+    // this.loginSignupService.getUserPage(this.userId).subscribe((result) => {
+    //   console.log(result);
+    // });
   }
 }
